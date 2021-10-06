7121a2de56561472cfb29e40862dc5816f4333d0
cheeseNA <kurocat2000@gmail.com>
2021-10-06 22:19:36 +0900

    Update background.js to redirect YouTube to random cat

--- background.js
+++ background.js
@@ -1,3 +1,37 @@
 chrome.runtime.onInstalled.addListener(() => {
   console.log('onInstalled fired');
 });
+
+chrome.webNavigation.onHistoryStateUpdated.addListener((details) => {
+  if (details.frameId === 0) {
+    console.log('onHistoryStateUpdated fired');
+    console.log(details);
+
+    if (details.url.search(/https:\/\/www\.youtube\.com/) !== -1) {
+      console.log('pattern matched');
+      chrome.scripting.executeScript({
+        target: { tabId: details.tabId },
+        function: redirectToCat,
+      });
+    }
+  }
+});
+
+chrome.webNavigation.onCommitted.addListener((details) => {
+  if (details.frameId === 0) {
+    console.log('onCommitted fired');
+    console.log(details);
+
+    if (details.url.search(/https:\/\/www\.youtube\.com/) !== -1) {
+      console.log('pattern matched');
+      chrome.scripting.executeScript({
+        target: { tabId: details.tabId },
+        function: redirectToCat,
+      });
+    }
+  }
+});
+
+function redirectToCat() {
+  location.replace('https://cataas.com/cat/gif');
+}
