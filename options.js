515fd0610ad276df0f0a9134084ed8fc28a1b7f1
cheeseNA <kurocat2000@gmail.com>
2021-10-10 14:07:12 +0900

    Update option.js to sync rules in a better way

--- options.js
+++ options.js
@@ -1,5 +1,7 @@
 const lifeMax = 30;
+let rules;
 
+// add rule to items dom
 function addItem(rule) {
   console.log('added rule: ');
   console.log(rule);
@@ -11,7 +13,14 @@ function addItem(rule) {
   newLi.appendChild(document.createTextNode('  '));
 
   const select = createSelect(rule.life);
-  select.addEventListener('change', updateChromeStorage);
+  select.addEventListener('change', (event) => {
+    const liArray = Array.from(document.querySelectorAll('#items>li'));
+    const index = liArray.indexOf(newLi);
+    rules[index].life = parseInt(event.target.value);
+
+    chrome.storage.sync.set({ rules: rules });
+    console.log(`rule changed: ${JSON.stringify(rules)}`);
+  });
   newLi.appendChild(select);
   newLi.appendChild(document.createTextNode('  '));
 
@@ -19,8 +28,14 @@ function addItem(rule) {
   deleteLink.textContent = '[Delete]';
   deleteLink.href = '#';
   deleteLink.addEventListener('click', (event) => {
+    // update rules and dom
+    const liArray = Array.from(document.querySelectorAll('#items>li'));
+    const index = liArray.indexOf(newLi);
+    rules.splice(index, 1);
     document.getElementById('items').removeChild(newLi);
-    updateChromeStorage();
+
+    chrome.storage.sync.set({ rules: rules });
+    console.log(`rule deleted: ${JSON.stringify(rules)}`);
   });
   newLi.appendChild(deleteLink);
 
@@ -33,22 +48,18 @@ function addButtonListener() {
   const site = addText.value;
   const domainPattern = /[0-9a-zA-Z\-:]+\.[0-9a-zA-Z\-:]+/;
   if (domainPattern.test(site)) {
-    addItem({ site: site, life: parseInt(addSelect.value) });
-    updateChromeStorage();
+    const newRule = { site: site, life: parseInt(addSelect.value) };
+    rules.push(newRule);
+    addItem(newRule);
+
+    chrome.storage.sync.set({ rules: rules });
   } else {
     console.log('invalid domain');
   }
   addText.value = '';
 }
 
-function constructItems() {
-  chrome.storage.sync.get(['rules'], function (result) {
-    for (const rule of result.rules) {
-      addItem(rule);
-    }
-  });
-}
-
+// create select (with options) dom
 function createSelect(selectedValue) {
   const select = document.createElement('select');
   for (i = 0; i <= lifeMax; i++) {
@@ -63,29 +74,22 @@ function createSelect(selectedValue) {
   return select;
 }
 
-function updateChromeStorage() {
-  const rules = [];
-  const ruleLis = document.querySelectorAll('#items>li');
-  for (const li of ruleLis) {
-    rules.push({
-      site: li.querySelector('span').textContent,
-      life: parseInt(li.querySelector('select').value),
-    });
-  }
-  console.log('rules: ');
-  console.log(rules);
-  chrome.storage.sync.set({ rules: rules });
-}
-
 document.addEventListener('DOMContentLoaded', function () {
+  // construct add form
   document
     .querySelector('#addButton')
     .addEventListener('click', addButtonListener);
-  constructItems();
-
   const addDiv = document.querySelector('#addDiv');
   const addSelect = createSelect(0);
   addSelect.name = 'addSelect';
   addSelect.id = 'addSelect';
   addDiv.insertBefore(addSelect, document.getElementById('addButton'));
+
+  // construct rules
+  chrome.storage.sync.get(['rules'], function (result) {
+    rules = result.rules;
+    for (const rule of result.rules) {
+      addItem(rule);
+    }
+  });
 });
